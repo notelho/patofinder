f96a7834185938fa3139468b6930f0561d74025e
enbot <nimbotelho@gmail.com>
2020-05-16 18:09:57 -0300

    feat: add basic dictionary structure

---
+++ src/utils/types-dictionary.ts
@@ -0,0 +1,46 @@
+export const typesDictionary = {
+
+    "img": {
+        "type": "filter",
+        "extensions": [
+            "svg",
+            "png",
+            "gif",
+            "jpg",
+        ]
+    },
+
+    "audio": {
+        "type": "filter",
+        "extensions": [
+            "mp3"
+        ]
+    },
+
+    "video": {
+
+    },
+
+    "stream": {
+        "type": "deep"
+    },
+
+    "js": {
+        "type": "filter",
+        "extensions": [
+            ".min.js",
+            ".js",
+        ]
+    },
+
+    "css": {
+        "type": "filter",
+        "extensions": [
+            ".min.css",
+            ".css",
+        ]
+    }
+
+}
+
+export default typesDictionary;
