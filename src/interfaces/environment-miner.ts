fe6141176f6a7848dbcef4fe71c927734e9d3a4e
enbot <nimbotelho@gmail.com>
2020-05-18 19:17:10 -0300

    feat: create filter preferences

--- src/interfaces/environment-miner.ts
+++ src/interfaces/environment-miner.ts
@@ -1,12 +1,15 @@
 import TypeExtensions from "./type-extensions";
 import EnvironmentDefault from "./environment-default";
 import FilterRule from './filter-rule';
+import FilterPreferences from "./filter-preferences";
 
 export interface EnvironmentMiner extends EnvironmentDefault {
 
     analyzer: 'miner';
 
     extensions: TypeExtensions;
+
+    ignore: FilterPreferences;
 
     filterRule: FilterRule;
 
---
+++ src/interfaces/filter-preferences.ts
@@ -0,0 +1,24 @@
+import TypeExtensions from "./type-extensions";
+import TypePath from "./type-path";
+import SearchType from "./search-type";
+
+export interface FilterPreferences {
+
+    ignoringTypes: SearchType[];
+
+
+
+    ignoringKeys: string[];
+
+    TypeExtensions | TypePath | string[];
+
+    .ignoreSearchType(['img', "css", "php"])
+    .ignoreCustomTypes(['.min.js', '.min', '.json'])
+    .ignoreSearchKeys(['google', 'analytics', 'store', 'apple', 'cdnjs', 'cloudflare', 'jquery', 'ajax', 'm2d'])
+    .orderByKeys(['stream', 'player', ';'])
+    .getSourcePaths();
+
+
+}
+
+export default FilterPreferences;
