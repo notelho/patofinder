db83a537306df8caa43710d2fa7cdc47a1a7bded
enbot <nimbotelho@gmail.com>
2020-05-20 01:27:52 -0300

    fix: streaming embracing rule

--- src/utils/dictionary/stream.ts
+++ src/utils/dictionary/stream.ts
@@ -36,20 +36,20 @@ export const stream: EnvironmentMiner = {
                 let streamingPoints: number = 0;
 
                 const rules = [
-                    { name: "isValidFormat", value: (extensions.indexOf(`.${streaming.codec_name}`) !== -1) },
-                    { name: "hasntStartTime", value: (typeof streaming.start_time === "undefined") },
-                    { name: "hasntStartPts", value: (typeof streaming.start_pts === "undefined") },
-                    { name: "hasntDuration", value: (typeof streaming.duration === "undefined") },
-                    { name: "hasntDurationTs", value: (typeof streaming.duration_ts === "undefined") },
+                    { points: 4, name: "isValidFormat", value: (extensions.indexOf(`.${streaming.codec_name}`) !== -1) },
+                    { points: 1, name: "hasntStartTime", value: (typeof streaming.start_time === "undefined") },
+                    { points: 1, name: "hasntStartPts", value: (typeof streaming.start_pts === "undefined") },
+                    { points: 1, name: "hasntDuration", value: (typeof streaming.duration === "undefined") },
+                    { points: 1, name: "hasntDurationTs", value: (typeof streaming.duration_ts === "undefined") },
                 ]
 
                 for (const rule of rules) {
                     if (rule.value) {
-                        streamingPoints++;
+                        streamingPoints += rule.points;
                     }
                 }
 
-                if (streamingPoints >= 3) {
+                if (streamingPoints >= 6) {
                     return true;
                 }
             }
