2b6ce688c8bec1da874229d4777ea63272f42154
enbot <nimbotelho@gmail.com>
2020-05-23 03:33:10 -0300

    fix: add ignoring rule to relative paths

--- src/utils/regexp/relative-path.ts
+++ src/utils/regexp/relative-path.ts
@@ -1,3 +1,4 @@
-export const relativePathRegexp = new RegExp(/^(\.?\.?\/)+/ig);
+// export const relativePathRegexp = new RegExp(/^(\.?\.?\/)+/ig);
+export const relativePathRegexp = new RegExp(/^(\.?\.?\/)+[a-z0-9]?[^<>;=-]/ig);
 
 export default relativePathRegexp;
