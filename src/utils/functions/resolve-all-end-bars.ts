a942a8d176b2a9444e15cc98a13713648e942d81
enbot <nimbotelho@gmail.com>
2020-05-23 19:13:25 -0300

    feat: add resolver for end bars

---
+++ src/utils/functions/resolve-all-end-bars.ts
@@ -0,0 +1,17 @@
+import TypePath from '../../interfaces/type-path';
+import resolveEndBar from './resolve-end-bar';
+
+export function resolveAllEndBars(paths: TypePath[]) {
+
+    const resolvedPaths: TypePath[] = [];
+
+    for (const path of paths) {
+
+        resolvedPaths.push(resolveEndBar(path));
+
+    }
+
+    return resolvedPaths;
+}
+
+export default resolveAllEndBars;
---
+++ src/utils/functions/resolve-end-bar.ts
@@ -0,0 +1,10 @@
+import TypePath from '../../interfaces/type-path';
+import endBarRegexp from '../regexp/end-bar';
+
+export function resolveEndBar(path: TypePath) {
+
+    return (path.match(endBarRegexp) ? path.slice(0, -1) : path) as TypePath;
+
+}
+
+export default resolveEndBar;
