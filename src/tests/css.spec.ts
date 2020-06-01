657bd4fc264aeb901a78c61fd88782209578663d
enbot <nimbotelho@gmail.com>
2020-06-01 20:01:53 -0300

    feat: setup jest tests

---
+++ src/tests/css.spec.ts
@@ -0,0 +1,18 @@
+// const valid = '';
+// const invalid = '';
+
+// describe("using patofinder search for css in a page", () => {
+
+//     it('should find one or more css files', () => {
+
+//         expect(xesque).toBe(desque);
+
+//     });
+
+//     it('throw a request error', () => {
+
+//         expect(arith.mul(1, 1)).toBe(1);
+
+//     });
+
+// });
