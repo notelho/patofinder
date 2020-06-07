4707d40d2bf0972e1ea4163b71befafd6ad2c4f3
enbot <nimbotelho@gmail.com>
2020-06-07 01:53:23 -0300

    fix: exporting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,3 @@
-// export * as interfaces from './public/interfaces';
-// export * as regexp from './public/regexp';
-// export * from './public/find';
-
-import find from './public/find';
-
-async function test() {
-
-    console.log('===================================================\n');
-    const url2 = 'http://www.radiorock.com.br/player';
-    // const url2 = 'http://youtube.com';
-    console.log(url2);
-    // const matches2 = await find(url2, 'stream');
-    const matches2 = await find(url2, 'javascript');
-    console.log(matches2);
-    console.log('===================================================\n');
-
-    // "test": "npm run test:build && npm run test:install && npm run test:cli",
-
-    // console.log('===================================================\n');
-    // const url4 = 'http://radiomixfm.com.br/';
-    // console.log(url4 + '\n');
-    // const matches4 = await find(url4, 'stream');
-    // console.log(matches4 + '\n');
-    // console.log('===================================================\n');
-
-}
-
-test();
+export * as interfaces from './public/interfaces';
+export * as regexp from './public/regexp';
+export * from './public/find';
--- src/tests/video.spec.ts
+++ src/tests/video.spec.ts
@@ -3,3 +3,29 @@ test('sample', () => {
     expect('true').toBe('true');
 
 });
+
+// import find from './public/find';
+
+// async function test() {
+
+//     console.log('===================================================\n');
+//     const url2 = 'http://www.radiorock.com.br/player';
+//     // const url2 = 'http://youtube.com';
+//     console.log(url2);
+//     // const matches2 = await find(url2, 'stream');
+//     const matches2 = await find(url2, 'javascript');
+//     console.log(matches2);
+//     console.log('===================================================\n');
+
+//     // "test": "npm run test:build && npm run test:install && npm run test:cli",
+
+//     // console.log('===================================================\n');
+//     // const url4 = 'http://radiomixfm.com.br/';
+//     // console.log(url4 + '\n');
+//     // const matches4 = await find(url4, 'stream');
+//     // console.log(matches4 + '\n');
+//     // console.log('===================================================\n');
+
+// }
+
+// test();
